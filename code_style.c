14dec0f98df4b3aa2882623c6d7335647f0ff5b8
sopka13 <sopka13@mail.ru>
2023-08-31 10:14:48 +0300

    new

--- code_style.c
+++ code_style.c
@@ -1,6 +1,45 @@
+// Code style file
 
+// COMMENTS:
+// [example] first line comment only:
+// SPDX-License-Identifier: GPL-2.0-only
 
+// [example] multiple lines comment:
+/*
+ *  first line
+ *  second line
+ */
+
+// [example] header single line comment:
+/*
+ *  Header and naming
+ */
+
+// [example] paragraph single line header:
+/* Sequence name */
+
+// [example] endline comment:
+#define SUCCESS		0		/* define for success result */
+
+// [example] multiline comment in func block:
+void	func_01(void)
+{
+	/* 
+	 * This is first line
+	 * second line
+	 */
+	return ;
+}
+
+// [example] temporary commented lines of code:
+// #define DEBUG
+
+// FUNCTIONS:
+// [example] function definition
 void	main(void)
 {
 	return 0;
 }
+
+// [example] function declaration
+void	func(void);
---
+++ elements.c
@@ -0,0 +1,1 @@
+// Elements for programming kernel modules
